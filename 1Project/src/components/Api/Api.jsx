8a35dc503b644e58bd95d47e64fee894ef33243c
Yuvi <singhraj0461@gmail.com>
2023-12-18 18:06:59 +0530

    Basic foundation has done

---
+++ 1Project/src/components/Api/Api.jsx
@@ -0,0 +1,5 @@
+const Api = [
+  {
+    //here is something that is called data
+  },
+];
---
+++ 1Project/src/components/OursTours/OursTours.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const OursTours = () => {
+  return (
+    <>
+      <div className="flex  justify-center my-12">
+        <h1 className=" text-center text-3xl  font-bold border-b-2 border-gray-500   ">
+          Our Tours
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default OursTours;
